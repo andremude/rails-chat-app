f56b6df0443aa09ca6b375695b4d3324d0e1dd83
Andres Munoz <andremude@hotmail.com>
2022-03-23 22:49:41 -0500

    rails chat app

---
+++ app/models/room.rb
@@ -0,0 +1,6 @@
+class Room < ApplicationRecord
+  validates_uniqueness_of :name
+  has_many :messages
+  scope :public_rooms, -> { where(is_private: false) }
+  after_create_commit {broadcast_append_to "rooms"}
+end
